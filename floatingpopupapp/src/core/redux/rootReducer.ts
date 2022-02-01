2b56e1b08abf04ed7c5c48b599bf46483891ca83
Umka1993 <58582062+Umka1993@users.noreply.github.com>
2022-02-01 22:47:29 +0200

    Delete floatingpopupapp directory

--- floatingpopupapp/src/core/redux/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from '@reduxjs/toolkit';
-import {userSlice} from '../../reduxModule/users/usersDataSlice';
-
-export const rootReducer = combineReducers({
-  users: userSlice,
-});
